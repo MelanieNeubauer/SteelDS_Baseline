477673c319ab7b8dee8fce0c5bc1ca43d9c48897
Christian Rauch <Christian.Rauch@unileoben.ac.at>
2026-09-23 11:02:03 +0200

    use common variable for 'results' folder

--- main_yolo.py
+++ main_yolo.py
@@ -19,7 +19,9 @@ def main_yolo(dataset='_a1'):
     models_to_test = ["yolov8n-seg.yaml", "yolo26n-seg.yaml", "yolo11n-seg.yaml","yolo12n-seg.yaml"]
     seeds = [1337, 7, 42, 99, 123]
 
-    os.makedirs(f"results{dataset}", exist_ok=True)
+    results_folder = f"results{dataset}"
+
+    os.makedirs(results_folder, exist_ok=True)
 
     for model_name in models_to_test:
         print(f"\n{'='*50}")
@@ -46,12 +48,12 @@ def main_yolo(dataset='_a1'):
             all_eval_metrics.append(metrics_row)
 
             # Save all metrics bundled in ONE single file (updated after each seed)
-            eval_csv_path = f"results{dataset}/{model_name}_evaluation_metrics_5_seeds.csv"
+            eval_csv_path = f"{results_folder}/{model_name}_evaluation_metrics_5_seeds.csv"
             pd.DataFrame(all_eval_metrics).to_csv(eval_csv_path, index=False)
 
             # 3. Copy results
             results_csv = os.path.join(run_dir, "results.csv")
-            destination_csv = f"results{dataset}/{model_name}_seed_{seed}_train_results.csv"
+            destination_csv = f"{results_folder}/{model_name}_seed_{seed}_train_results.csv"
 
             if os.path.exists(results_csv):
                 shutil.copy(results_csv, destination_csv)
@@ -60,7 +62,7 @@ def main_yolo(dataset='_a1'):
             # Generate plot
             plot_training_results(
                 csv_path=results_csv,
-                save_path=f"results{dataset}/{model_name}_seed_{seed}_training_plot.png"
+                save_path=f"{results_folder}/{model_name}_seed_{seed}_training_plot.png"
             )
 
             # Clean up RAM and VRAM (if GPU is used)
@@ -72,7 +74,7 @@ def main_yolo(dataset='_a1'):
 
         # Save evaluation summary as CSV
         eval_df = pd.DataFrame(all_eval_metrics)
-        eval_csv_path = f"results{dataset}/{model_name}_evaluation_metrics_5_seeds.csv"
+        eval_csv_path = f"{results_folder}/{model_name}_evaluation_metrics_5_seeds.csv"
         eval_df.to_csv(eval_csv_path, index=False)
         print(f"📊 Evaluation data across all 5 seeds for {model_name} saved in: {eval_csv_path}")
 
